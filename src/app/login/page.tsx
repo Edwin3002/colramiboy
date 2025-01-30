f2652a5c6ff1ddfc15ae39c03bcc2c7da9c8d611
Edwin Vargas Ayala <edwin.dev3002@gmail.com>
2025-01-30 14:02:05 -0500

    feat: configure store and microserviceApi

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@
 import Button from "@/components/ui/buttons/Button";
 import FiledInput from "@/components/ui/inputs/FiledInput";
 import useAuthHook from "@/modules/auth/hooks/useAuthHook";
-import { decrement, increment } from "@/providers/redux/slices/counterSlices";
+// import { decrement, increment } from "@/providers/redux/slices/counterSlices";
 import { RootState } from "@/providers/redux/store";
 import { Formik, Form } from "formik";
 import { useDispatch, useSelector } from "react-redux";
@@ -12,17 +12,15 @@ import { toFormikValidationSchema } from "zod-formik-adapter";
 
 const validationSchema = z.object({
   username: z.string().min(1, "El nombre de usuario es obligatorio"),
-  password: z
-    .string()
-    .min(6, "La contraseña debe tener al menos 6 caracteres")
-    .refine((value) => value.trim() == "", {
-      message: "La contraseña es obligatoria",
-    }),
+  password: z.string().min(6, "La contraseña debe tener al menos 6 caracteres"),
+  // .refine((value) => value.trim() == "", {
+  //   message: "La contraseña es obligatoria",
+  // }),
 });
 const Login = () => {
-  const { login } = useAuthHook();
-  const count = useSelector((state: RootState) => state.counter.value);
-  const dispatch = useDispatch();
+  const { login, isLoading } = useAuthHook();
+  // const count = useSelector((state: RootState) => state.counter.value);
+  // const dispatch = useDispatch();
   return (
     <div className="flex justify-center items-center min-h-screen ">
       <div className="w-lg max-w-md bg-[#ead1ac] dark:bg-gray-800 p-8 rounded-lg shadow-lg flex flex-col justify-center">
@@ -43,6 +41,7 @@ const Login = () => {
                   label="Usuario"
                   placeholder="Ingresa tu nombre de usuario*"
                   value={values.username}
+                  helperText={errors.username}
                   onChange={handleChange}
                 />
               </div>
@@ -54,32 +53,41 @@ const Login = () => {
                   placeholder="Ingresa tu contraseña*"
                   value={values.password}
                   onChange={handleChange}
+                  helperText={errors.password}
                   type="password"
                 />
               </div>
-              <Button variant="contained" color="blue" size="md" type="submit">
-                Iniciar sesión
-              </Button>
+              <div className="flex justify-center">
+                <Button
+                  disabled={isLoading}
+                  variant="contained"
+                  color="blue"
+                  size="md"
+                  type="submit"
+                >
+                  Iniciar sesión
+                </Button>
+              </div>
             </Form>
           )}
         </Formik>
-      <div>
-        <div>
-          <button
-            aria-label="Increment value"
-            onClick={() => dispatch(increment())}
-          >
-            Increment
-          </button>
-          <span>{count}</span>
-          <button
-            aria-label="Decrement value"
-            onClick={() => dispatch(decrement())}
-          >
-            Decrement
-          </button>
-        </div>
-      </div>
+        {/* <div>
+          <div>
+            <button
+              aria-label="Increment value"
+              onClick={() => dispatch(increment())}
+            >
+              Increment
+            </button>
+            <span>{count}</span>
+            <button
+              aria-label="Decrement value"
+              onClick={() => dispatch(decrement())}
+            >
+              Decrement
+            </button>
+          </div>
+        </div> */}
       </div>
     </div>
   );
